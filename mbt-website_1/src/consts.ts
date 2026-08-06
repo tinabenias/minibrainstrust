e6f86c77a012aac9f6d54032cf7f83423f4df5b4
tinabenias <tinabenias@gmail.com>
2026-08-06 16:49:38 +0930

    Add files via upload

--- mbt-website_1/src/consts.ts
+++ mbt-website_1/src/consts.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = 'Mini Brains Trust';
 export const SITE_DESCRIPTION =
   'A future-ready kindy curriculum you run at home. Ready-to-go daily play for Australian 3 to 5 year olds that grows the eight human strengths a screen can’t teach.';
-export const SITE_URL = 'https://minibrainstrust.com';
+export const SITE_URL = 'https://minibrainstrust.com.au';
